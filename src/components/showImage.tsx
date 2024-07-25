435060861d7873404adffb3d42452ae536d8bc39
Akbar Ali <m.akberali507@gmail.com>
2024-07-25 22:28:11 +0500

    Add Dropdown menu & download option

--- src/components/showImage.tsx
+++ src/components/showImage.tsx
@@ -4,6 +4,17 @@ import CloudinaryImage from "./cloudinary-image";
 import ImageSkeleton from "@/components/image-skeleton";
 import { Suspense } from "react";
 import Link from "next/link";
+import { Button } from "./ui/button";
+import VerticalThreeDot from "./vertical-three-dot";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 type SearchResult = {
   width: number;
@@ -46,15 +57,45 @@ function ImageResults() {
     <main>
       <div className="flex gap-5 flex-wrap">
         {results.map((result: SearchResult) => (
-          <div key={result.public_id}>
-            <Link href={`/api/download?public_id=${result.public_id}`}>
-              <CloudinaryImage // Get Cloudinary Image Component and show image using public id
-                src={result.public_id}
-                width={result.width}
-                height={result.height}
-                url=""
-              />
-            </Link>
+          <div key={result.public_id} className="relative">
+            <CloudinaryImage // Get Cloudinary Image Component and show image using public id
+              src={result.public_id}
+              width={result.width}
+              height={result.height}
+              url=""
+            />
+            <div className="absolute top-3 right-3">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="secondary"
+                    className="p-3 rounded-full opacity-60"
+                  >
+                    <VerticalThreeDot className="rounded-full" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-36">
+                  <DropdownMenuLabel>Options</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <span>Edit</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Link
+                        href={`/api/download?public_id=${result.public_id}`}
+                      >
+                        <span>Download</span>
+                      </Link>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <span>Delete</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
           </div>
         ))}
       </div>
