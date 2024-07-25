cfb07577c5ade72c1836becfd8bf56695b961e02
Akbar Ali <m.akberali507@gmail.com>
2024-07-25 22:35:36 +0500

    Some changes on album.tsx and favourite.tsx

--- src/app/album/page.tsx
+++ src/app/album/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Album() {
   return (
     <>
-      <h1>Album</h1>
+      <h1 className="flex justify-center items-center min-h-96 w-full text-2xl font-bold">
+        Comming Soon
+      </h1>
     </>
   );
 }
--- src/app/favourite/page.tsx
+++ src/app/favourite/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Favourite() {
   return (
     <>
-      <h1>Favourite</h1>
+      <h1 className="flex justify-center items-center min-h-96 w-full text-2xl font-bold">
+        Comming Soon
+      </h1>
     </>
   );
 }
